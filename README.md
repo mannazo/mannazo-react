0b8d7b99452442cb6843f98ed48086205852371d
Joonhoe Kim <26rote@gmail.com>
2024-06-17 11:07:56 +0900

    docs: readme 추가 및 예제 추가

--- README.md
+++ README.md
@@ -1,70 +1,55 @@
-# Getting Started with Create React App
+# 프로젝트 개발환경
+## 생성 방법
+- [Create React App](https://create-react-app.dev/) 사용
+## 주요 라이브러리 및 도구
+- 라우팅 처리: React Router
+- UI 컴포넌트 라이브러리: Material-UI
+- 폼 처리: Formik, React Hook Form
+- 상태 관리 라이브러리: Redux
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+### 버전 및 디펜던시
 
-## Available Scripts
+```json
+  "dependencies": {
+    "@testing-library/jest-dom": "^5.17.0",
+    "@testing-library/react": "^13.4.0",
+    "@testing-library/user-event": "^13.5.0",
+    "react": "^18.3.1",
+    "react-dom": "^18.3.1",
+    "react-router-dom": "^6.23.1",
+    "react-scripts": "5.0.1",
+    "styled-components": "^6.1.11",
+    "web-vitals": "^2.1.4"
+  }
+```
 
-In the project directory, you can run:
+## 패키지 구조
+```
+my-app
+├── README.md
+├── node_modules
+├── package.json
+├── .gitignore
+├── public
+│   ├── favicon.ico
+│   ├── index.html
+│   ├── logo192.png
+│   ├── logo512.png
+│   ├── manifest.json
+│   └── robots.txt
+└── src
+    ├── App.css
+    ├── App.js
+    ├── App.test.js
+    ├── index.css
+    ├── index.js
+    ├── logo.svg
+    ├── serviceWorker.js
+    └── setupTests.js
+```
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## 개발환경 설정
+### Node.js 및 npm 설치 가이드
+### IDE 설정
+#### 확장 프로그램 목록
+### Convention
--- src/App.js
+++ src/App.js
@@ -1,117 +1,88 @@
-import { useState } from 'react';
-import './App.css';
+import React from 'react';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { createStore } from 'redux';
+import './style.css';
 
-function Header(props) {
-  console.log('props', props);
-  return (    <header>
-    <h1>
-      <a href="/" onClick={(event) => {
-        event.preventDefault();
-        props.onChangeMode();
-      }}>{props.title}</a>
-    </h1>
-  </header>)
-}
-
-function Nav(props) {
-  const lis = [];
-  for(let i=0; i<props.topics.length; i++) {
-    let t= props.topics[i];
-    lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event => {
-      event.preventDefault();
-      props.onChangeMode(Number(event.target.id));
-    }}>{t.title}</a></li>);
-  }
-  return (
-    <nav>
-      <ol>
-        {lis}
-      </ol>
-    </nav>
-  )
-}
-
-function Article(props) {
-  return (
-    <article>
-      <h2>
-        {props.title}
-      </h2>
-      {props.body}
-    </article>
-  )
-}
-
-function Create(props) {
-  return (
-    <article>
-      <h2>Create</h2>
-      <form onSubmit={event=>{
-        event.preventDefault();
-        const title = event.target.title.value;
-        const body = event.target.body.value;
-        props.onCreate(title, body);
-      }}>
-        <p><input type="text" name="title" placeholder="title"></input></p>
-        <p><textarea name="body" placeholder="body"></textarea></p>
-        <p><input type="submit" value="Create"></input></p>
-      </form>
-    </article>
-
-  )
-}
-function App() {
-  const _mode = useState('WELCOME');
-  console.log('_mode', _mode);
-  //mode, setMode는 임의로 설정한 이름일 뿐이다.
-  const [mode, setMode] = useState('WELCOME');
-  const [id, setId] = useState(null);
-  const [nextId, setNextId] = useState(4);
-  const [topics, setTopics] = useState([
-    {id:1, title:'html', body:'html is...'},
-    {id:2, title:'css', body:'css is...'},
-    {id:3, title:'javascript', body:'javascript is...'},
-  ]);
-
-  let content = null;
-  if(mode==='WELCOME') {
-    content = <Article title ="WELCOME" body="Hello, Web"></Article>
-  } else if(mode==='READ') {
-    let title, body = null;
-    for(let i=0; i<topics.length; i++) {
-      console.log(topics[i].id, id);
-      if(topics[i].id===id) {
-        title = topics[i].title;
-        body = topics[i].body;
-      }
-    }
-    content = <Article title={title} body={body}></Article>
-  } else if(mode=='CREATE') {
-    content = <Create onCreate={(_title, _body)=>{
-      const newTopic = {id:nextId, title:_title, body:_body};
-      topics.push(newTopic);
-      // setTopics(topics);
-    }}></Create>
+function reducer(currentState, action) {
+  if (currentState === undefined) {
+    return { number: 1 };
   }
 
-  return (    <div>
-    <Header title="REACT" onChangeMode={() => {
-      // alert("header");
-      setMode('WELCOME');
-    }}></Header>
-    <Nav topics={topics} onChangeMode={(_id) => {
-      // alert(id);
-      setMode('READ');
-      setId(_id);
-      }}></Nav>
-      {content}
-      <a href="/create" onClick={event=>{
-        event.preventDefault();
-        setMode('CREATE');}}>
-        Create</a>
-    {/* <Article title="Welcome" body="Hello, Web"></Article> */}
-  </div>);
-}
+  const newState = { ...currentState };
 
-export default App;
+
+  if(action.type==='PLUS') {
+    newState.number++;
+  }
+
+  return newState;
+}
+const store = createStore(reducer);
+
+export default function App() {
+  return (
+    <div id="container">
+      <h1>Root</h1>
+      <div id="grid">
+        <Provider store={store}>
+          <Left1></Left1>
+          <Right1></Right1>
+        </Provider>
+      </div>
+    </div>
+  );
+}
+function Left1(props) {
+  return (
+    <div>
+      <h1>Left1 </h1>
+      <Left2></Left2>
+    </div>
+  );
+}
+function Left2(props) {
+  return (
+    <div>
+      <h1>Left2 : </h1>
+      <Left3></Left3>
+    </div>
+  );
+}
+function Left3() {
+  console.log('L3');
+  const number = useSelector((state) => state.number);
+
+  return (
+    <div>
+      <h1>Left3: {number}</h1>
+    </div>
+  );
+}
+function Right1(props) {
+  return (
+    <div>
+      <h1>Right1</h1>
+      <Right2></Right2>
+    </div>
+  );
+}
+function Right2(props) {
+  return (
+    <div>
+      <h1>Right2</h1>
+      <Right3></Right3>
+    </div>
+  );
+}
+function Right3(props) {
+  console.log('R3');
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Right3</h1>
+      <input type="button" value="+" onClick={() => {
+        dispatch({ type: 'PLUS'})
+      }}></input>
+    </div>
+  );
+}
