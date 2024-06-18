8f3625270e23f3ce2e697220a7e39a7a1f0042c5
hl6vbw <hl6vbw@virginia.edu>
2024-06-18 17:54:39 +0900

    Collect input values

--- src/Pages/SignupScreen.tsx
+++ src/Pages/SignupScreen.tsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignupScreen: React.FC = () => {
-  return (
-    <div>
-      <div>
-        <label htmlFor="name">이름</label>
-        <input id="name" type="text" placeholder="이름(실명)을 입력해주세요" />
-        {/* <span>이름을 입력해주세요.</span> */}
-      </div>
-      <div>
-        <label htmlFor="email">이메일</label>
-        <input id="email" type="email" placeholder="example@soongo.com" />
-        {/* <span>이메일 주소를 입력해주세요.</span> */}
-      </div>
-      <div>
-        <label htmlFor="password">비밀번호</label>
-        <input
-          id="password"
-          type="password"
-          placeholder="영문,숫자 조합 8자리 이상 입력해주세요. 꼭!"
-        />
-        {/* <span>비밀번호를 입력해주세요.</span> */}
-      </div>
-      <button>다음</button>
-    </div>
-  );
-};
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const userInfo = {
+        name,
+        email,
+        password
+    }
+
+    const handleSubmit = () => {
+        console.log(name, email, password);
+    }
+
+    return (
+        <div>
+            <div>
+                <label htmlFor="name">이름</label>
+                <input id="name" type="text" placeholder="이름(실명)을 입력해주세요" value={name}
+                       onChange={e => setName(e.target.value)}/>
+
+            </div>
+            <div>
+                <label htmlFor="email">이메일</label>
+                <input id="email" type="email" placeholder="example@soongo.com" value={email}
+                       onChange={e => setEmail(e.target.value)}/>
+
+            </div>
+            <div>
+                <label htmlFor="password">비밀번호</label>
+                <input id="password" type="password" value={password} onChange={e => setPassword(e.target.value)}/>
+            </div>
+            <button onClick={handleSubmit}>다음</button>
+            <p>{JSON.stringify(userInfo)}</p>
+
+        </div>
+    );
+}
 
 export default SignupScreen;
