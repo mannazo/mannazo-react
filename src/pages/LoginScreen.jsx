a2e2d4b284f9613e2c59b10e6b6f63d634e29a84
hl6vbw <hl6vbw@virginia.edu>
2024-06-20 14:32:01 +0900

    login screen google icon added

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,19 +1,44 @@
-// {
-//   "email": "string",
-//   "password": "string",
-// }
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { FcGoogle } from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
+import Logo from '/vite.svg';
 
 const LoginScreen = () => {
+  const responseGoogle = (response) => {};
   return (
-    <div>
-      <h1>LoginScreen.tsx</h1>
-      <div>
+    <div className='flex justify-center items-center flex-col h-screen'>
+      <div className='p-5'>
+        <img src={Logo} />
+      </div>
+
+      <div className='p-5'>
         <label htmlFor='email'>이메일</label>
         <input id='email' name='email' type='email' placeholder='example@soongo.com' />
       </div>
+
       <div>
         <label htmlFor='password'>비밀번호</label>
         <input id='password' name='password' type='password' />
+      </div>
+      <div className='shadow-2xl'>
+        <GoogleLogin
+          clientId=''
+          render={(renderProps) => (
+            <button
+              type='button'
+              className='bg-mainColor flex justify-items-center p-3 rounded-lg'
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <FcGoogle className='mr-4' />
+              Sign in
+            </button>
+          )}
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy='single_host_origin'
+        ></GoogleLogin>
       </div>
       <button>이메일로 로그인</button>
       <button>카카오로 시작</button>
