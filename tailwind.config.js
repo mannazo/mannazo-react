56ee27ddceb16dfc726dc934848da335816ec7ce
Joonhoe Kim <26rote@gmail.com>
2024-06-24 20:39:30 +0900

    fix: content 오류 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html, ./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html,",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
