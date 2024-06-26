bf55b4851ab51d4e6583e110478526c7750422b6
hl6vbw <hl6vbw@virginia.edu>
2024-06-26 17:59:28 +0900

    signupscreen added

--- src/pages/Signup2Screen.jsx
+++ src/pages/Signup2Screen.jsx
@@ -1,95 +1,90 @@
-// {
-//   "nationality": "integer",
-//   "language": "string",
-//   "profile_photo": "string",
-//   "bio": "string",
-//   "residence": "string",
-//   "role": "string",
-// }
+
+import {useEffect, useState} from 'react';
+import axios from "axios";
+import {API_SERVER} from "../constants/paths.js";
+
 const Signup2Screen = () => {
+  const [error, setError] = useState('');
+  const [storedUserInfo, setStoredUserInfo] = useState(JSON.parse(localStorage.getItem('fetchCodeResponse')));
+
+
+  // useEffect(() => {
+  //   setStoredUserInfo();
+  // }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Updated:")
+    console.log(storedUserInfo)
+    axios.put(API_SERVER + "/api/v1/user", storedUserInfo)
+        .then(response => console.log(response.data))
+        .catch(error => {
+          console.error(error);
+          if (error.response) {
+
+            console.error('Error:', error.response.data);
+          }
+        });
+  };
+
+  function handleChange(event) {
+    setStoredUserInfo({
+      ...storedUserInfo,
+      [event.target.name]:event.target.value
+
+    })
+  }
+
   return (
-    <div>
-      <h1>Signup2Screen.tsx</h1>
-      <form>
+    <div className='Signup2Form'>
+      <h1>Sign up</h1>
+      <form onSubmit={handleSubmit}>
         <div>
-          <label>
-            Birthday:
-            <select name='year'>
-              <option value=''>Select year of Birth</option>
-              <option value='2000'>2000</option>
-              <option value='1999'>1999</option>
-              <option value='1998'>1998</option>
-              <option value='1997'>1997</option>
-              <option value='1996'>1996</option>
-              <option value='1996'>...</option>
-            </select>
-            <select name='Month'>
-              <option value=''>Select month of Birth</option>
-              <option value='1'>1</option>
-              <option value='2'>2</option>
-              <option value='3'>3</option>
-              <option value='4'>4</option>
-              <option value='5'>5</option>
-              <option value='6'>6</option>
-              <option value='7'>7</option>
-              <option value='8'>8</option>
-              <option value='9'>9</option>
-              <option value='10'>10</option>
-              <option value='11'>11</option>
-              <option value='12'>12</option>
-            </select>
-          </label>
+          <label>Name:</label>
+          <input type='text' name="name" required onChange={handleChange}/>
         </div>
+        {/*<div>*/}
+        {/*  <label>Birthday:</label>*/}
+        {/*  <input type='date' required onChange={(e) => setBirthday(e.target.value)}/>*/}
+        {/*</div>*/}
         <div>
           <label>MBTI</label>
-          <select name='mbti'>
-            <option value=''>MBTI</option>
-            <option value='1'>ENFP</option>
-            <option value='2'>ENFJ</option>
-            <option value='3'>ENTP</option>
-            <option value='4'>ENTJ</option>
-            <option value='5'>ESFP</option>
-            <option value='6'>ESTP</option>
-            <option value='7'>ESFJ</option>
-            <option value='8'>ESTJ</option>
-            <option value='9'>INFP</option>
-            <option value='10'>...</option>
+          <input type='text' name="mbti" onChange={handleChange}/>
+        </div>
+        <div>
+          <label>Nationality</label>
+          <select name="nationality" onChange={handleChange}>
+            <option value='Korea'>Korea</option>
+            <option value='United States'>United States</option>
           </select>
         </div>
         <div>
-          <label>거주지역</label>
-          <select name='MBTI'>
-            <option value=''>...</option>
+          <label>Language</label>
+          <select name='language' onChange={handleChange}>
+            <option value='English'>영어</option>
+            <option value='Chinese'>중국어</option>
+            <option value='Korean'>한국어</option>
           </select>
         </div>
-        <div>
-          <label>언어</label>
-          <select name='mbti'>
-            <option value=''>영어</option>
-            <option value=''>중국어</option>
-            <option value=''>한국어</option>
-          </select>
-        </div>
-        <div>
-          <label>이미지</label>
-          <input type='file' name='profilePhoto' />
-        </div>
+        {/*<div>*/}
+        {/*  <label>이미지</label>*/}
+        {/*  <input type='file' name='profilePhoto' onChange={handleImage}/>*/}
+        {/*</div>*/}
         <div>
           <label>Interests</label>
-          <select name='interests'>
-            <option value=''>Food</option>
-            {/* <option value="">Tour</option> */}
-            <option value=''>...</option>
-          </select>
+          <input type='text' name='interests' required onChange={handleChange}/>
         </div>
         <div>
           <label>
             소개글:
-            <textarea name='introduction' />
+            <textarea name='introduction' required onChange={handleChange}/>
           </label>
         </div>
 
         <button>회원가입 완료</button>
+        {error && <div>{error}</div>}
+        <p>{JSON.stringify(storedUserInfo)}</p>
+        {/*<p>{uuid}</p>*/}
       </form>
     </div>
   );
