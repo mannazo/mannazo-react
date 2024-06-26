193365d6c849e34d837863024884a55f7e780e8c
chlorella71 <chlorella71@naver.com>
2024-06-26 17:24:37 +0900

    feat:마이페이지 구현중

--- src/pages/mypage/AccountSetting.jsx
+++ src/pages/mypage/AccountSetting.jsx
@@ -1,66 +1,110 @@
 import { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import AccountSettingModify from './AccountSettingModify.jsx'
+import AccountSettingModify from './AccountSettingModify.jsx';
+import axios from 'axios';
 
 const AccountSetting = () => {
   // 사용자가 계정 변경 버튼 눌렀는지에 대한 상태
   const [isModifyClicked, setIsModifyClicked] = useState(false);
+
   // 유저의 데이터를 로컬스토리지에서 가져와서 초기화
   const [userData, setUserData] = useState(JSON.parse(localStorage.getItem('fetchCodeResponse')));
-  // true
-  const [iscomplite, setIscomplite] = useState(false);
+  true;
+
+  // const [userData, setUserData] = useState(null); // 초기값을 null로 설정
+
+  const [isComplete, setIsComplete] = useState(false);
+
+  console.log(userData);
+
+  useEffect(() => {
+    // 로컬스토리지에서 userId 값을 가져옵니다.
+    const userId = JSON.parse(localStorage.getItem('fetchCodeResponse')).user_id;
+
+    // userId가 존재하면 데이터를 가져오는 API 요청을 보냅니다.
+    if (userId) {
+      // 컴포넌트가 마운트될 때 한 번 실행됩니다.
+      // 여기서 서버에서 데이터를 가져옵니다.
+      const fetchUserData = async () => {
+        try {
+          const response = await axios.get('http://api.mannazu.com/api/v1/user/{userDate.userId}'); // 실제 API 엔드포인트로 변경해야 합니다.
+          setUserData(response.data); // 서버에서 받은 데이터를 상태에 저장합니다.
+        } catch (error) {
+          console.error('서버에서 사용자 데이터를 가져오는 중 오류 발생:', error);
+          // 오류 처리 (예: 사용자에게 오류 메시지 표시)
+        }
+      };
+
+      fetchUserData(); // 데이터 가져오기 함수 호출
+    }
+
+    // 로컬 스토리지에서 데이터를 가져오는 것과는 달리 비동기 함수를 사용하여 서버에서 데이터를 가져옵니다.
+  }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정합니다.
 
   console.log(userData);
 
   return (
-      <>
-        <div>
-          <h1 style={{fontSize: '20px'}}>계정설정</h1>
-        </div>
-        {isModifyClicked ?
-            <AccountSettingModify setIsModifyClicked={setIsModifyClicked} userData={userData} setIscomplite={setIscomplite}/>
-            :
+    <>
+      <div>
+        <h1 style={{ fontSize: '20px' }}>계정설정</h1>
+      </div>
+      {userData ? (
+        // userData가 null이 아니면(서버에서 데이터를 받은 경우)
+        isModifyClicked ? (
+          <AccountSettingModify
+            setIsModifyClicked={setIsModifyClicked}
+            userData={userData}
+            setIsComplete={setIsComplete}
+          />
+        ) : (
+          <div>
             <div>
-              <div>
-                <label htmlFor='name'>이름: {userData.name}</label>
-              </div>
-              <div>
-                <label htmlFor='email'>이메일: {userData.email}</label>
-              </div>
-              <div>
-                <label htmlFor='password'>비밀번호: {userData.password}</label>
-              </div>
-              <div>
-                <label htmlFor='nickname'>닉네임: {userData.nickname}</label>
-              </div>
-              <div>
-                <label htmlFor='nationality'>국적: {userData.nationality}</label>
-              </div>
-              <div>
-                <label htmlFor='language'>언어: {userData.language}</label>
-              </div>
-              <div>
-                <label htmlFor='introduction'>소개: {userData.introduction}</label>
-              </div>
-              <div>
-                <label htmlFor='city'>거주지: {userData.city}</label>
-              </div>
-              <div>
-                <label htmlFor='gender'>성별: {userData.gender}</label>
-              </div>
-              <div>
-                <label htmlFor='mbti'>MBTI: {userData.mbti}</label>
-              </div>
-              <div>
-                <label htmlFor='interests'>흥미: {userData.interests}</label>
-              </div>
-              <button onClick={() => {
-                setIsModifyClicked(true)
-              }}>계정변경
-              </button>
+              <label htmlFor='name'>이름: {userData.name}</label>
             </div>
-        }
-      </>
+            <div>
+              <label htmlFor='email'>이메일: {userData.email}</label>
+            </div>
+            <div>
+              <label htmlFor='password'>비밀번호: {userData.password}</label>
+            </div>
+            <div>
+              <label htmlFor='nickname'>닉네임: {userData.nickname}</label>
+            </div>
+            <div>
+              <label htmlFor='nationality'>국적: {userData.nationality}</label>
+            </div>
+            <div>
+              <label htmlFor='language'>언어: {userData.language}</label>
+            </div>
+            <div>
+              <label htmlFor='introduction'>소개: {userData.introduction}</label>
+            </div>
+            <div>
+              <label htmlFor='city'>거주지: {userData.city}</label>
+            </div>
+            <div>
+              <label htmlFor='gender'>성별: {userData.gender}</label>
+            </div>
+            <div>
+              <label htmlFor='mbti'>MBTI: {userData.mbti}</label>
+            </div>
+            <div>
+              <label htmlFor='interests'>흥미: {userData.interests}</label>
+            </div>
+            <button
+              onClick={() => {
+                setIsModifyClicked(true);
+              }}
+            >
+              계정변경
+            </button>
+          </div>
+        )
+      ) : (
+        // userData가 null인 경우(서버에서 데이터를 아직 받지 않은 경우)
+        <p>Loading...</p>
+      )}
+    </>
   );
 };
 
--- src/pages/mypage/AccountSettingModify.jsx
+++ src/pages/mypage/AccountSettingModify.jsx
@@ -2,102 +2,127 @@ import React, {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 
-const AccountSettingModify = ({setIsModifyClicked, userData, setIscomplite}) => {
+const AccountSettingModify = ({setIsModifyClicked, userData, setIsComplete}) => {
+
+    const [formData, setFormData] = useState({
+        name: userData.name || '', // 기본값으로 빈 문자열 설정
+        email: userData.email || '',
+        password: userData.password || '',
+        nickname: userData.nickname || '',
+        nationality: userData.nationality || '',
+        language: userData.language || '',
+        introduction: userData.introduction || '',
+        city: userData.city || '',
+        gender: userData.gender || '',
+        mbti: userData.mbti || '',
+        interests: userData.interests || '',
+    });
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        // 새로운 객체를 생성하여 userData 상태를 업데이트
+        setFormData({
+            ...formData,
+            [id]: value,
+        });
+    };
+
+    const handleSave = async () => {
+        try {
+            // 서버 API 엔드포인트
+            const apiUrl = 'http://api.mannazu.com/api/v1/user';
+
+            // API 요청 보낼 데이터
+            const response = await axios.put(apiUrl, formData);
+
+            // 요청이 성공적으로 처리된 경우
+            console.log('서버 응답:', response.data);
+
+            // 완료 상태 관리 (필요에 따라 setIsComplete 사용)
+            setIsModifyClicked(false);
+            setIsComplete(true);
+        } catch (error) {
+            // 요청이 실패한 경우
+            console.error('서버 요청 중 오류 발생:', error);
+            // 오류 처리 (예: 사용자에게 알림 등)
+        }
+    };
+
     return(
         <>
             <div>
                 <label htmlFor='name'>이름: </label>
-                <input type='text' id='name' value={userData.name}
-                    onChange={(e) => {
-                        userData.name = e.target.value;
-                    }}
+                <input type='text' id='name' value={formData.name}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='email'>이메일: </label>
-                <input type='email' id='email' value={userData.email}
-                    onChange={(e) => {
-                        userData.email = e.target.value;
-                    }}
+                <input type='email' id='email' value={formData.email}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='password'>비밀번호: </label>
-                <input type='password' id='password' value={userData.password}
-                    onChange={(e) => {
-                        userData.password = e.target.value;
-                    }}
+                <input type='password' id='password' value={formData.password}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='nickname'>닉네임: </label>
-                <input type='text' id='nickname' value={userData.nickname}
-                    onChange={(e) => {
-                        userData.nickname = e.target.value;
-                    }}
+                <input type='text' id='nickname' value={formData.nickname}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='nationality'>국적: </label>
-                <input type='text' id='nationality' value={userData.nationality}
-                    onChange={(e) => {
-                        userData.nationality = e.target.value;
-                    }}
+                <input type='text' id='nationality' value={formData.nationality}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='language'>언어: </label>
-                <input type='text' id='language' value={userData.language}
-                    onChange={(e) => {
-                        userData.language = e.target.value;
-                    }}
+                <input type='text' id='language' value={formData.language}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='introduction'>소개: </label>
-                <input type='text' id='introduction' value={userData.introduction}
-                    onChange={(e) => {
-                        userData.introduction = e.target.value;
-                    }}
+                <input type='text' id='introduction' value={formData.introduction}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='city'>거주지: </label>
-                <input type='text' id='city' value={userData.city}
-                    onChange={(e) => {
-                        userData.city = e.target.value;
-                    }}
+                <input type='text' id='city' value={formData.city}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='gender'>성별: </label>
-                <input type='text' id='gender' value={userData.gender}
-                    onChange={(e) => {
-                        userData.gender = e.target.value;
-                    }}
+                <input type='text' id='gender' value={formData.gender}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='mbti'>MBTI: </label>
-                <input type='text' id='mbti' value={userData.mbti}
-                    onChange={(e) => {
-                        userData.mbti = e.target.value;
-                    }}
+                <input type='text' id='mbti' value={formData.mbti}
+                       onChange={handleChange}
                 />
             </div>
             <div>
                 <label htmlFor='interests'>흥미: </label>
-                <input type='text' id='interests' value={userData.interests}
-                    onChange={(e) => {
-                        userData.interests = e.target.value;
-                    }}
+                <input type='text' id='interests' value={formData.interests}
+                       onChange={handleChange}
                 />
             </div>
-            <button onClick={() => {
-                setIsModifyClicked(false);
-                setIscomplite(true);
-            }}>완료
-            </button>
+            <button onClick={handleSave}>완료</button>
+
+        {/*<button onClick={() => {*/}
+        {/*    setIsModifyClicked(false);*/}
+        {/*    setIscomplite(true);*/}
+        {/*}}>완료*/}
+            {/*</button>*/}
         </>
     )
 }
