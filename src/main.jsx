986d63ba4a521f7998eca3bc5a1bcd4046b38211
Joonhoe Kim <26rote@gmail.com>
2024-06-28 02:37:07 +0900

    fix: build 오류 해결 (react-buddy 플러그인이 생성한 경로와 import 제거)

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { DevSupport } from '@react-buddy/ide-toolbox';
-import { ComponentPreviews, useInitial } from './dev/index.js';
 import { NextUIProvider } from '@nextui-org/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NextUIProvider>
       <BrowserRouter>
-
-          <App />
-
+        <App />
       </BrowserRouter>
     </NextUIProvider>
   </React.StrictMode>,
